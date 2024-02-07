9d33b97ea711cbd55d6f210ff6858b092c2f5402
Mauro Ramos <mauro.dgramos@gmail.com>
2024-02-07 17:53:34 +0000

    [Update] Structure re-worked, adding initial JSON

--- jsFiles/main.js
+++ jsFiles/main.js
@@ -1,5 +1,5 @@
 window.onload = async () => {
-    const request = await fetch("jsFiles/test.json");
+    const request = await fetch("../test.json");
     const result = await request.json();
     console.log(result)
 
